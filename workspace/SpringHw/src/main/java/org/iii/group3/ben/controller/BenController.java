a96265aeac5f0f15520aa338447039d6a4f13ee0
Ben0729 <78082696+Ben0729@users.noreply.github.com>
2021-06-15 20:18:02 +0800

    addview

--- workspace/SpringHw/src/main/java/org/iii/group3/ben/controller/BenController.java
+++ workspace/SpringHw/src/main/java/org/iii/group3/ben/controller/BenController.java
@@ -1,9 +1,11 @@
 package org.iii.group3.ben.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@EnableTransactionManagement
 public class BenController {
 
 	@GetMapping("/ben")
